40102cfec424e71b8c5694f9e70ac7366e28ca69
antonybogun <antonybogun@gmail.com>
2016-07-25 14:45:16 -0400

    Implement GUI

--- COMP212-Group1_M16_Lab67/Form1.Designer.cs
+++ COMP212-Group1_M16_Lab67/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLives = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(519, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(572, 12);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lives:";
+            // 
+            // txtLives
+            // 
+            this.txtLives.Location = new System.Drawing.Point(572, 38);
+            this.txtLives.Name = "txtLives";
+            this.txtLives.ReadOnly = true;
+            this.txtLives.Size = new System.Drawing.Size(100, 20);
+            this.txtLives.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(519, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(522, 111);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(150, 150);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(522, 359);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(150, 150);
+            this.btnPlayAgain.TabIndex = 0;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(683, 521);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtLives);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Guess the Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLives;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnPlayAgain;
     }
 }
 
--- COMP212-Group1_M16_Lab67/Form1.cs
+++ COMP212-Group1_M16_Lab67/Form1.cs
@@ -15,6 +15,33 @@ namespace COMP212_Group1_M16_Lab67
         public Form1()
         {
             InitializeComponent();
+
+            System.Windows.Forms.Button[] buttons = new System.Windows.Forms.Button[100];
+            int locationX = 10, locationY = 10;
+
+            for (int i=0;i<10;i++)
+            { 
+                for(int j=0;j<10;j++)
+                {
+                    int index = i * 10 + j;
+                    buttons[index] = new Button();
+                    buttons[index].Size = new System.Drawing.Size(50, 50);
+                    buttons[index].Location = new System.Drawing.Point(locationX, locationY);
+                    locationX += buttons[index].Size.Width;
+                    buttons[index].Text = (index + 1).ToString();
+                    buttons[index].Click += Button_Click;
+                    this.Controls.Add(buttons[index]);
+                }
+                locationX = 10;
+                locationY += buttons[0].Size.Height;
+            }
+
+
         }
-    }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string buttonText = ((Button)sender).Text;
+
+        }
+}
 }
